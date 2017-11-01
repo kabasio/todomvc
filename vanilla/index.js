1a8602fd717ddeb35f95c947328e982f2a42be50
kabashima shiori <sor.0810@gmail.com>
2017-11-01 22:17:33 +0900

    add list

--- vanilla/index.js
+++ vanilla/index.js
@@ -11,13 +11,51 @@ if (question === 'fine') {
 
 const input = document.getElementsByClassName('new-todo') [0] ;
 
-console.log(input);
+function createNewTask (name){
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.className = 'view';
+
+  const input = document.createElement('input');
+  input.setAttribute('type','checkbox');
+
+  const label = document.createElement('label');
+  label.value = name;
+
+  const button = document.createElement('button');
+  button.className = 'destroy';
+
+  div.appendChild(input);
+  div.appendChild(label);
+  div.appendChild(button);
+
+  const edit = document.createElement('input');
+  edit.className = 'edit';
+
+  li.appendChild(div);
+  li.appendChild(edit);
+
+
+  return li;
+}; 
+
 
 input.addEventListener('keydown', function(e) {
-  console.log(e);
   if (e.keyCode === 13) {
-    alert('できたよ');
+    createNewTask();
+    const newTask = input.value;
+    const oya = document.getElementsByClassName('todo-list')[0];
+    const  li  = createNewTask(newTask);
+    oya.appendChild(li);
+    console.log(newTask);
+
+    /* やりたいこと
+ 1.フォームの値を取得する ◎
+ 2.新しいliをつくる
+ 3.todo-rist(ul)に、上記で作成したliを入れる
+ 4.liに取得した値を入れる(data-idってやつがひもづいてる
+    console.log(newTask);
+    */
   }
 });
-
 
