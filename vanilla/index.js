dc97a517c5809284bd7a731543237854981964bc
kabashima shiori <sor.0810@gmail.com>
2017-11-17 13:11:59 +0900

    create Store vo1

--- vanilla/index.js
+++ vanilla/index.js
@@ -5,7 +5,6 @@ function createNewTodo(name) {
   const $div = document.createElement('div');
   $div.className = 'view';
   
-
   const $inputToggle = document.createElement('input');
   $inputToggle.setAttribute('type','checkbox');
   $inputToggle.className = 'toggle';
@@ -38,12 +37,96 @@ function createNewTodo(name) {
   return $li;
 };
 
-$inputNewTodo.addEventListener('keydown', function(e) {
+$inputNewTodo.addEventListener('keydown', f unction(e) {
   if (e.keyCode === 13) {
     const newTodo = $inputNewTodo.value;
+    // store.addTodo(newTodo)
     const $ulTodoList = document.getElementsByClassName('todo-list')[0];
     const $li = createNewTodo(newTodo);
     $ulTodoList.appendChild($li);
     $inputNewTodo.value = '';
   }
 });
+
+/* todos で　タスクをためておく配列を作る
+
+const todos = [
+  { 
+    id: 000,
+    label: 'ほげ',
+    isCompleted: true,
+  },
+  { 
+    id: 001,
+    label: 'ほげ',
+    isCompleted: true,
+  },
+  { 
+    id: 002,
+    label: 'ほげ',
+    isCompleted: false,
+  },
+];
+*/
+function CreateStore () {
+    const todos = [];
+    const $ulTodoList = document.querySelector('.todo-list');
+    let satus = 'SHOW_ALL';
+
+    const render = function() {
+      let filterdTodos;
+      
+      if (satus === 'SHOW_ACTIVE') {
+        filterdTodos = todos.filter(function(todo) {return !todo.isCompleted});
+      } else if (satus === 'SHOW_COMPRETED') {
+        filterdTodos = todos.filter(function(todo) {return todo.isCompleted});
+      } else {
+        filterdTodos = todos;
+      }
+    }
+  // ul.todo-listの中身を削除
+  while($ulTodoList.firstChild === null) {
+    $ulTodoList.removeChild($ulTodoList.firstChild);
+  }
+  //for文 前から 初期化・処理の前・処理の後
+  for (let i = 0; i < filterdTodos.length; i++) {
+    const $li = createNewTodo(filterdTodos[i].label);
+    $ulTodoList.appendChild($li);
+  }
+};
+
+const addTodo = function(label) {
+  const todo = { 
+    label: label,
+    isCompleted: false,
+  },
+  todos = todos.push(todo);
+  render();
+};
+
+const showAll = function() {
+  status = 'SHOW-ALL';
+  render();
+};
+
+const showActive = function() {
+  status = 'SHOW-ACTIVE';
+  render();
+};
+
+const showCompereted = function() {
+  status = 'SHOW=COMPLETED';
+  render();
+};
+
+return {
+  addTodo: addTodo,
+  showAll: showAll,
+  showActive: showActive,
+  showCompereted: showCompereted,
+};
+
+const store = CreateStore();
+store.showAll();
+store.showActive();
+store.showCompereted();
